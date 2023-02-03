194f263e397fcb302e24f99e919120d92fbb64ff
Haitham <haitham@stretch.com>
2023-02-03 06:54:30 +0400

    added user registration auth

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 from models import *
+from auth import CryptContext, pwd_context, get_hashed_password
+
+# Signals
+from tortoise.signals import post_save
+from typing import List, Optional, Type
+from tortoise import BaseDBAsyncClient
 
 app = FastAPI()
+
+
+@post_save(User)
+async def create_business(
+        sender: "Type[User]",
+        instance: User,
+        created: bool,
+        using_db: "Optional[BaseDBAsyncClient]",
+        update_fields: List[str]
+) -> None:
+
+    if created:
+        business_obj = await Business.create(
+            business_name=instance.username,
+            owner=instance
+        )
+        await business_pydantic.from_tortoise_orm(business_obj)
+        # Send the email
+
+@app.post("/registration")
+async def user_registrations(user: user_pydanticIn):
+    user_info = user.dict(exclude_unset=True)
+    user_info["password"] = get_hashed_password(user_info["password"])
+    user_obj = await User.create(**user_info)
+    new_user = await user_pydantic.from_tortoise_orm(user_obj)
+    return {
+        "status": "ok",
+        "data": f"Hello {new_user.username} , thanks for choosing our services. Please check your email inbox and "
+                f"click on the link to conirm your registration."
+    }
 
 
 @app.get("/")
